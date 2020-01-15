0ef5be14b691c4029c82b8ff7278248b874a9da1
Mark Bailey <mabailey78@yahoo.co.uk>
2020-01-15 14:10:32 +0000

    Added build files

---
+++ precache-manifest.11ef31a7afcfa42abaae88ab12c4a0fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b545f3a4ecaca6214273ecb4796b37f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f28f4fc454f46afe9131",
+    "url": "/static/js/2.3bdd01ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3bdd01ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5117782506c1699b1f0f",
+    "url": "/static/js/main.0a5d9db3.chunk.js"
+  },
+  {
+    "revision": "fb422f0a218364c605cb",
+    "url": "/static/js/runtime-main.59b5f7c0.js"
+  }
+]);
