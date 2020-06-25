16b275bf2992c1452dd73496a63bc5258783a60e
Mark Bailey <markbailey78@yahoo.co.uk>
2020-06-26 00:44:59 +0100

    Added temporary splash page

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const App = () => {
-  return <>{/*  */}</>;
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#333',
+        color: '#fff',
+        textTransform: 'uppercase',
+        fontFamily: 'helvetica',
+        textAlign: 'center',
+      }}
+    >
+      <img src="/mb-logo.png" alt="My Logo" />
+      <div style={{ marginTop: -56 }}>
+        <h1 style={{ margin: 0 }}>Mark Bailey</h1>
+        <small>Javascript / React Developer</small>
+      </div>
+    </div>
+  );
 };
 
 export default App;
